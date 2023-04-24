e844a39babd09a9ae9da1104166736fea2f9250c
bobby chennamsetti <tejatech116@gmail.com>
2023-04-24 16:00:40 +0530

    terraform inputs..

--- servers.tf
+++ servers.tf
@@ -1,3 +1,11 @@
+data "aws_ami" "centos" {
+  most_recent      = true
+  name_regex       = "Centos-8-Devops-Practice"
+  owners           = ["269533346076"]
+}
+output "ami" {
+  value = "data.aws_ami.centos.image_id"
+}
 resource "aws_instance" "frontend" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
